8a552db0d11a25a92ff78d9b94d04629caabbfd8
Miroslav Vatov <mirovatov@gmail.com>
2020-12-01 15:01:29 +0200

    Deleted the exercises and exams from Basics loder

--- Python Basics/More exercises 01 - 03 целзий - фаренхайт.py
+++
@@ -1,3 +0,0 @@
-celsius = float(input())
-to_fahrenheit = (celsius * 1.8) + 32
-print(f'{to_fahrenheit:.2f}')
--- Python Basics/More exercises 01 - 09 Weather forecast.py
+++
@@ -1,6 +0,0 @@
-forecats  = input()
-
-if forecats == 'sunny':
-    print(f'It\'s warm outside!')
-else:
-    print(f'It\'s cold outside!')
