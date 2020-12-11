d32f9bbb94e8d2b95dd80c74f904b66dd1f7f65d
Miroslav Vatov <mirovatov@gmail.com>
2020-12-11 17:03:49 +0200

    Deleted Lists Basic folder/ Created new one

--- Python Fundamentals 2020 - 2021/04 - Lists Basics/Lab - 04 - 01 ver 2.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-lst1 = []
-for i in range(n):
-    name = input()
-
-    lst1 += [name]
-
-print(lst1, end='')
--- Python Fundamentals 2020 - 2021/04 - Lists Basics/Lab - 04 - 01.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-lst1 = []
-for i in range(n):
-    name = input()
-    lst = [name]
-
-    lst1 += lst
-print(lst1, end='')
