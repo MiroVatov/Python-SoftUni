7424bf3d517f2180fe2ea1200197bed8b555deee
Miroslav <mirovatov@gmail.com>
2021-03-08 11:21:14 +0200

    Add files via upload

---
+++ Python Advanced 2021/FUNCTIONS-ADVANCED/Exercise 09.py
@@ -0,0 +1,26 @@
+def sum_numbers(num1, num2):
+    return num1 + num2
+
+
+def multiply_numbers(num1, num2):
+    return num1 * num2
+
+
+def func_executor(*args):
+
+    results_list = []
+
+    for func_definition in args:
+        func, func_args = func_definition
+        return_value = func(*func_args)
+        results_list.append(return_value)
+
+    return results_list
+
+
+print(
+    func_executor(
+        (sum_numbers, (1, 2)),
+        (multiply_numbers, (2, 4))
+    )
+)
