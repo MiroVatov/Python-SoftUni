77221dc514853b30006bd409dff84e8d33129301
Miroslav <mirovatov@gmail.com>
2021-03-10 10:29:23 +0200

    Add files via upload

---
+++ Python Advanced 2021/FILE_HANDLING/Labs/Lab 03.py
@@ -0,0 +1,14 @@
+# VARIANT 1
+
+with open('my_first_file.txt', 'w') as f:
+    f.write('I just created my first file')
+
+# VARIANT 2
+
+# f = open('my_first_file.txt', 'w')
+# try:
+#     f.write('I just created my first file')
+# finally:
+#     f.close()
+
+
