fe681e45e3fee99d516b935875553bfc0b445ec8
Miroslav <mirovatov@gmail.com>
2021-03-30 11:08:00 +0300

    Add files via upload

---
+++ PYTHON OOP/ENCAPSULATION/Labs/email_validator.py
@@ -0,0 +1,31 @@
+class EmailValidator:
+
+    def __init__(self, min_length: int, mails: list, domains: list):
+        self.min_length = min_length
+        self.mails = mails
+        self.domains = domains
+
+    def __validate_name(self, name):
+        return len(name) >= self.min_length
+
+    def __validate_mail(self, mail):
+        return mail in self.mails
+
+    def __validate_domain(self, domain):
+        return domain in self.domains
+
+    def validate(self, email):
+        name, rest = email.split('@')
+        email, domain = rest.split('.')
+        if self.__validate_name(name) and self.__validate_mail(email) and self.__validate_domain(domain):
+            return True
+        return False
+
+
+mails = ["gmail", "softuni"]
+domains = ["com", "bg"]
+email_validator = EmailValidator(6, mails, domains)
+print(email_validator.validate("pe77er@gmail.com"))
+print(email_validator.validate("georgios@gmail.net"))
+print(email_validator.validate("stamatito@abv.net"))
+print(email_validator.validate("abv@softuni.bg"))
---
+++ PYTHON OOP/ENCAPSULATION/Labs/person.py
@@ -0,0 +1,26 @@
+class Person:
+
+    def __init__(self, name: str, age: int):
+        self.__name = name
+        self.__age = age
+
+    def get_name(self):
+        return self.__name
+
+    def get_age(self):
+        return self.__age
+
+    # @property
+    # def get_name(self):
+    #     return self.__name
+    #
+    # @ property
+    # def get_age(self):
+    #     return self.__age
+
+
+person = Person("George", 32)
+print(person.get_name())
+print(person.get_age())
+# print(person.get_name)
+# print(person.get_age)
