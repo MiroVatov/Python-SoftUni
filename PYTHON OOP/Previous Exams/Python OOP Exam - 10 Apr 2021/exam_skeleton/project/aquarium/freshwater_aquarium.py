edc2da751430bc7a43d597da8af61d76204d11d6
Miroslav <mirovatov@gmail.com>
2021-04-13 15:05:33 +0300

    Delete freshwater_aquarium.py

--- PYTHON OOP/Previous Exams/Python OOP Exam - 10 Apr 2021/exam_skeleton/project/aquarium/freshwater_aquarium.py
+++
@@ -1,9 +0,0 @@
-from project.aquarium.base_aquarium import BaseAquarium
-
-
-class FreshwaterAquarium(BaseAquarium):
-
-    def __init__(self, name):
-        super().__init__(name, capacity=50)
-        self.aquarium_fish_type = "FreshwaterFish"
-
