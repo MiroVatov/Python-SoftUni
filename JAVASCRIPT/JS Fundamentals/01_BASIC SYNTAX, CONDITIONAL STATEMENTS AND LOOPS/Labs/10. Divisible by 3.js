87edf1cbde37080d084036402582a4c058257643
Miroslav <mirovatov@gmail.com>
2021-05-30 11:03:25 +0300

    Delete JAVASCRIPT/JS Fundamentals/01_BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS directory

--- JAVASCRIPT/JS Fundamentals/01_BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS/Labs/10. Divisible by 3.js
+++
@@ -1,8 +0,0 @@
-function divisiblebyThree() {
-    let num = 1
-    for (i = num; i <= 100; i++)
-        if (i % 3 === 0)
-            console.log(i)
-}
-
-divisiblebyThree()
--- JAVASCRIPT/JS Fundamentals/01_BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS/Labs/3. Numbers from 1 to 5.js
+++
@@ -1,7 +0,0 @@
-function solve() {
-    let end = 5
-    for (let i = 1; i <= end; i ++) {
-        console.log(i)
-    }
-}
-solve()
