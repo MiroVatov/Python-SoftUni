05c08bd47080466e478198b091b8039ae3edb5b7
Miroslav <mirovatov@gmail.com>
2021-10-06 13:39:31 +0300

    Add files via upload

---
+++ PYTHON OOP/Previous Exams/Python OOP Retake Exam - 23 August 2021/01. Structure_Skeleton/project/astronaut/project/astronaut/biologist.py
@@ -0,0 +1,12 @@
+from project.astronaut.astronaut import Astronaut
+
+
+class Biologist(Astronaut):
+
+    def __init__(self, name: str):
+        super(Biologist, self).__init__(name=name, oxygen=70)
+
+    def breathe(self):
+        self.oxygen -= 5
+
+
